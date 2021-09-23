d91469b47b835d025d32a293bf3f27b5ccfccad4
Muhammad Umair Arshad <imua8850@gmail.com>
2021-09-23 20:19:08 +0500

    Update Covid-19 Project_Queries.sql

--- Covid-19 Project_Queries.sql
+++ Covid-19 Project_Queries.sql
@@ -3,69 +3,125 @@ Queries used for Covid-19 Project
 */
 
 
-
--- Global Numbers Deaths
-
-select SUM(CAST(new_cases as int)) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as float))/SUM(CAST(New_Cases as float))*100 as DeathPercentage
-from CovidProject..CovidDeaths
--- Where location like '%states%'
+select * from CovidProject..CovidDeaths
 where continent <> ''
--- Group By date
+order by 3,5
+
+-- Select Data that we are going to be starting with
+
+select location, date, total_cases, new_cases, total_deaths, population
+from CovidProject..CovidDeaths
+where continent <> ''
 order by 1,2
 
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
 
--- Showing continents with the highest death count per population
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
+select location, date, total_cases, total_deaths, CAST(total_deaths as float)/nullif(CAST(total_cases as float),0)*100 as DeathPercentage
+from CovidProject..CovidDeaths
+where continent <> ''
+order by 1,2
 
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From CovidProject..CovidDeaths
---Where location like '%states%'
-Where continent = ''
-and location not in ('World', 'European Union', 'International')
-Group by location
-order by TotalDeathCount desc
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
 
+select location, date, population, total_cases, CAST(total_cases as float)/nullif(CAST(population as float),0)*100 as PercentPopulationInfected
+from CovidProject..CovidDeaths
+order by 1,2
 
 -- Countries with Highest Infection Rate compared to Population
 
-select location, population, MAX(total_cases) as HighestInfectionCount, MAX(CAST(total_cases as float)/nullif(CAST(population as float),0))*100 as PercentPopulationInfected
+select location, population, MAX(total_cases) as HighestInfectionCount, MAX(CAST(total_cases as float)/nullif(CAST(population as float),0)*100) as PercentPopulationInfected
 from CovidProject..CovidDeaths
--- where location like '%pakistan%'
 group  by location, population
 order by PercentPopulationInfected desc
 
+-- Countries with Highest Death Count per Population
 
--- Countries with Highest Infection Count and Percentage Population Infected by date 
+select location, MAX(CAST(total_deaths as int)) as TotalDeathCount
+from CovidProject..CovidDeaths
+where continent <> ''
+group by location
+order by TotalDeathCount desc
 
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  MAX(CAST(total_cases as float)/nullif(CAST(population as float),0))*100 as PercentPopulationInfected
-From CovidProject..CovidDeaths
---Where location like '%states%'
-Group by Location, Population, date
-order by PercentPopulationInfected desc
+-- BREAKING THINGS DOWN BY CONTINENT
 
--- Global Numbers Vaccinations
+-- Showing continents with the highest death count per population
 
-select MAX(cast(people_fully_vaccinated as float)) as PeopleFullyVaccinated, MAX(cast(people_vaccinated as float)) as PeopleRecievedFirstDose, MAX(cast(people_fully_vaccinated as float))/MAX(CAST(population as float))*100 as PercentageFullyVaccinated, MAX(CAST(people_vaccinated as float))/MAX(CAST(population as float))*100 as PercentageRecievedFirstDose
-from CovidProject..CovidDeaths dea
-join CovidProject..CovidVaccinations vac
-on dea.location = vac.location
-order by 1,2
-
--- Showing contintents with the vaccinations count per population
-
-select continent, MAX(cast(people_fully_vaccinated as float)) as PeopleFullyVaccinated, MAX(cast(people_vaccinated as float)) as PeopleRecievedFirstDose
-from CovidProject..CovidVaccinations
+select continent, MAX(CAST(total_deaths as int)) as TotalDeathCount
+from CovidProject..CovidDeaths
 where continent <> ''
 group by continent
-order by PeopleFullyVaccinated desc, PeopleRecievedFirstDose desc 
+order by TotalDeathCount desc
 
--- Countries with vaccinations count compared to Population
+-- GLOBAL NUMBERS
 
-select vac.location, population, MAX(cast(people_fully_vaccinated as float)) as PeopleFullyVaccinated, MAX(cast(people_vaccinated as float)) as PeopleRecievedFirstDose, MAX(cast(people_fully_vaccinated as float))/nullif(MAX(CAST(population as float)),0)*100 as PercentageFullyVaccinated, MAX(CAST(people_vaccinated as float))/nullif(MAX(CAST(population as float)),0)*100 as PercentageRecievedFirstDose
-from CovidProject..CovidVaccinations vac
-join CovidProject..CovidDeaths dea
-on dea.location = vac.location
-group  by vac.location, population
-order by PeopleFullyVaccinated desc, PeopleRecievedFirstDose desc
+select SUM(CAST(new_cases as int)) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as float))/SUM(CAST(New_Cases as float))*100 as DeathPercentage
+from CovidProject..CovidDeaths
+where continent <> ''
+order by 1,2
 
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From CovidProject..CovidDeaths dea
+Join CovidProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent <> '' 
+order by 2,3
+
+-- Using CTE to perform Calculation on Partition By in previous query
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+, SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From CovidProject..CovidDeaths dea
+Join CovidProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent <> ''
+)
+Select *, cast(RollingPeopleVaccinated as float)/nullif(cast(Population as float),0)*100
+From PopvsVac
+
+-- Using Temp Table to perform Calculation on Partition By in previous query
+
+DROP Table if exists #PercentPopulationVaccinated
+Create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date nvarchar(255),
+Population int,
+New_vaccinations int,
+RollingPeopleVaccinated int
+)
+
+Insert into #PercentPopulationVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+, SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From CovidProject..CovidDeaths dea
+Join CovidProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent <> ''
+
+
+Select *, cast(RollingPeopleVaccinated as float)/nullif(cast(Population as float),0)*100
+From #PercentPopulationVaccinated
+
+-- Creating View to store data for later visualizations
+
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From CovidProject..CovidDeaths dea
+Join CovidProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent <> ''
